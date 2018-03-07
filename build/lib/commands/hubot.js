537576948b1f6eb77c704683703c1ea31ed1f98c
superbiger <superbiger@qq.com>
2018-03-07 18:26:13 +0800

    add:hubot shell

--- build/lib/commands/hubot.js
+++ build/lib/commands/hubot.js
@@ -1,9 +1,12 @@
-
 const shelljs = require('shelljs')
 
 module.exports = (api, options) => {
   api.registerCommand('hubot', args => {
     const command = args._[0]
     console.log('hello hubot')
+    shelljs.exec('set -e')
+    shelljs.exec('yarn install')
+    shelljs.exec('export PORT=8081')
+    shelljs.exec('HUBOT_SLACK_TOKEN=${HUBOT_SLACK_TOKEN} node_modules/.bin/hubot --name "supergo" "$@" --adapter "slack"')
   })
 }
